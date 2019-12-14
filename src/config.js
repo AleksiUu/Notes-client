68911cc5534cb832eba023be06e54a5ed96cb8d6
Aleksi Uusitalo <aleksi.uusitalo@siili.com>
2019-12-14 22:20:55 +0200

    add aws amplify and configure it

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-1",
+    BUCKET: "notes-uploads-bucketti"
+  },
+  apiGateway: {
+    REGION: "eu-west-1",
+    URL: "https://o5kkjkhqob.execute-api.eu-west-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "eu-west-1",
+    USER_POOL_ID: "eu-west-1_jCFGaIEVQ",
+    APP_CLIENT_ID: "6pffds9cjdggusoakeu190t1ud",
+    IDENTITY_POOL_ID: "eu-west-1:d36b426f-010e-417d-9e30-60203fa28110"
+  }
+};
