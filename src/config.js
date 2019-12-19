f3c648e2e7095f46c8b314f8445ed48b80f65b78
Aleksi Uusitalo <aleksi.uusitalo@siili.com>
2019-12-19 18:30:25 +0200

    add netlify build script and use custom domain api

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export default {
   },
   apiGateway: {
     REGION: "eu-west-1",
-    URL: "https://fq4oadpwdj.execute-api.eu-west-1.amazonaws.com/dev"
+    URL: "https://api.aleksiuusitalo.com/dev"
   },
   cognito: {
     REGION: "eu-west-1",
